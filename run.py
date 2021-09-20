ac36544d6b1029ce2c2b1895b1e4bb069cd0a065
nojuskybartas <nojus.kybartas@gmail.com>
2021-09-20 16:31:35 +0200

    error fix??

--- run.py
+++ run.py
@@ -1,19 +1,19 @@
 from flask import Flask, render_template, Response, jsonify
 from app.main import gen_frames, det_emotion
 
-app = Flask(__name__)
+ai_app = Flask(__name__)
 
-@app.route('/')
+@ai_app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/video_feed')
+@ai_app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/emotion')
+@ai_app.route('/emotion')
 def emotion():
     return Response(det_emotion(), mimetype='text/event-stream')
 
 if __name__=="__main__":
-    app.run()
+    ai_app.run()
