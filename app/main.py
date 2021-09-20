6679a6bcdc981a11906c58f6999fc87a5e4349b6
nojuskybartas <nojus.kybartas@gmail.com>
2021-09-20 16:25:57 +0200

    restructure 3

--- app/main.py
+++ app/main.py
@@ -77,16 +77,6 @@ def det_emotion():
         
         sleep(0.5)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/emotion')
-def emotion():
-    return Response(det_emotion(), mimetype='text/event-stream')
 
 
